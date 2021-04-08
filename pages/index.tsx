57ad667b5b984bc7d063136b865dfc7e4db5771c
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-04-08 17:22:43 -0300

    set typescript and styled-components

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Formulário de Adesivos</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <div>Olar</div>
+    </div>
+  )
+}
